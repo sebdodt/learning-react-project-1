c34512c219a4b55ea64768cac3c8887807baebc3
sebdodt <mats.dodt@googlemail.com>
2022-07-21 13:38:03 +0300

    static page

--- index.js
+++ index.js
@@ -8,17 +8,52 @@ see if you can figure out how to render an <ul> with 2+ <li>s inside*/
 //     document.getElementById("root")
 // )
 
+// import ReactDOM from "react-dom/client"
+// import React from "react"
+// import { createRoot } from "react"
+
+// const navbar = (
+//     <nav>Navigation Bar
+//         <h1 className = 'header'>website</h1>
+//         <ul>
+//             <li>Pricing</li>
+//             <li>About</li>
+//             <li>Contact</li>
+//         </ul>
+//     </nav>
+// )
+
+// const page = (
+//     <div>
+//         <h1>My awesome website in React</h1>
+//         <h3>Reasons I love React</h3>
+//         <ol>
+//             <li>It's composable</li>
+//             <li>It's declarative</li>
+//             <li>It's a hireable skills</li>
+//         </ol>
+//     </div>
+// )
+
+// ReactDOM.render(page, document.getElementById('root'))
+
+// // const root = ReactDOM.createRoot(document.getElementById("root"))
+// // root.render(page)
 
 
-const navbar = (
-    <nav>Navigation Bar
-        <h1 className = 'header'>website</h1>
+
+// import React from "react"
+// import ReactDOM from "react-dom"
+
+const page = (
+    <div>
+        <img src="./react-logo.png" width = "40px" />
+        <h1>Fun facts about React</h1>
         <ul>
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
         </ul>
-    </nav>
+    </div>
 )
 
-ReactDOM.render(navbar, document.getElementById('root'))
+ReactDOM.render(page, document.getElementById("root"))
